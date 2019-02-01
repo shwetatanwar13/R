f3ffc1de53b35f3054da902a6e44fe5ad2652091
Shweta Tanwar <41698215+shwetatanwar13@users.noreply.github.com>
2019-01-31 16:34:09 -0800

    Basic R commands

---
+++ BasicR.R
@@ -0,0 +1,42 @@
+x<-10L
+typeof(x)
+
+a="Hello"
+b=1.0
+c<-paste(a,b)
+c
+typeof(b)
+4<5
+
+y=rnorm(1)
+if (y<1) {
+z<-"assigned"
+} else if (y>1){
+z<-"not assigned"
+}
+
+
+seq(1,15,2)
+
+1:15
+
+a <- c(1,2,3,4,5)
+a
+
+?rnorm(4,)
+
+
+w<-c(1,2,3,4,5)
+
+w[-11]
+
+
+x<-rnorm(5)
+x
+
+?factor
+
+
+
+
+
